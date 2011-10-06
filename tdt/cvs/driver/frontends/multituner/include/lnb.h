1a10a32346108fa42198c09b83c8213d16dd5907
pinky@asuslappy <al_borland33@yahoo.de>
2011-10-06 15:02:45 +0200

    cpufrequ src add

--- tdt/cvs/driver/frontends/multituner/include/lnb.h
+++
@@ -1,7 +0,0 @@
-#ifndef lnb_123
-#define lnb_123
-
-#define cLNB_LNBH221 1
-#define cLNB_PIO     2
-
-#endif
--- tdt/cvs/driver/simu_button/simu_button.h
+++
@@ -1,9 +0,0 @@
-/*
- */
-
-static short paramDebug = 1;
-#define TAGDEBUG "[simu_button] "
-
-#define dprintk(x...) do { if (paramDebug) printk(TAGDEBUG x); } while (0)
-
-
