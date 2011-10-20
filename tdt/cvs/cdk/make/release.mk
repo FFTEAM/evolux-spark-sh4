cebb3456f97e010e4ec705449cf601f254eb6f4e
pinky@asuslappy <al_borland33@yahoo.de>
2011-10-20 16:54:41 +0200

    spark-E2: del libeplayer2-stuff as unneeded

--- tdt/cvs/cdk/make/release.mk
+++ tdt/cvs/cdk/make/release.mk
@@ -256,8 +256,9 @@ endif
 	cp -f $(buildprefix)/root/usr/bin/mkfs.jffs2 $(prefix)/release/usr/bin/
 	cp -RP $(buildprefix)/root/usr/lib/liblzo2.so.2* $(prefix)/release/usr/lib/ 
 	cp -f $(buildprefix)/root/usr/bin/backup_pingulux_image.sh $(prefix)/release/usr/bin/
-### del libcoolstream stuff as not needed for pingulux-E2 ###
+### del libcoolstream+libeplayer2 stuff as not needed for pingulux-E2 ###
 	rm -f $(prefix)/release/lib/libcool*
+	rm -f $(prefix)/release/lib/libeplayer2*
 
 release_spark7162:
 	echo "spark7162" > $(prefix)/release/etc/hostname
