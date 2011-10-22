3f4ab4c36bb9ca9ecfba05f5690517857a1d99c8
pinky@asuslappy <al_borland33@yahoo.de>
2011-10-22 11:47:08 +0200

    duck merge

--- tdt/cvs/cdk/make/neutrino.mk
+++ tdt/cvs/cdk/make/neutrino.mk
@@ -80,6 +80,7 @@ neutrino-nightly-clean neutrino-nightly-distclean:
 	rm -f $(DEPDIR)/neutrino-nightly
 	rm -f $(DEPDIR)/neutrino-nightly.do_compile
 	rm -f $(DEPDIR)/neutrino-nightly.do_prepare
+	rm -rf $(appsdir)/neutrino-nightly.org
 	rm -rf $(appsdir)/neutrino-nightly
 
 
