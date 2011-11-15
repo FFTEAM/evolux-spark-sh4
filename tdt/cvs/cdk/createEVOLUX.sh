fe8e978e97158ba6da53ca017a89833d33e41ee0
~pinky1981 <al_borland33@yahoo.de>
2011-11-15 22:21:11 +0100

    script update

--- tdt/cvs/cdk/createEVOLUX.sh
+++ tdt/cvs/cdk/createEVOLUX.sh
@@ -5,6 +5,31 @@ E2DIR=$PINGUDIR/tufsbox/release_with_dev
 NEUTRINODIR=$PINGUDIR/tufsbox/release_neutrino_with_dev
 EVOLUXDIR=$PINGUDIR/tufsbox/release_EVOLUX_with_dev
 DESTINATION="/devel/E2"
+# originally created by schischu and konfetti
+# fedora parts prepared by lareq
+# fedora/suse/ubuntu scripts merged by kire pudsje (kpc)
+
+# make sure defines have not already been defined
+UBUNTU=
+FEDORA=
+SUSE=
+# Try to detect the distribution
+if `which lsb_release > /dev/null 2>&1`; then 
+	case `lsb_release -s -i` in
+		Debian*) UBUNTU=1; USERS="su -c";;
+		Fedora*) FEDORA=1; USERS="sudo";;
+		SUSE*)   SUSE=1;   USERS="su";;
+		Ubuntu*) UBUNTU=1; USERS="sudo";;
+	esac
+fi
+# Not detected by lsb_release, try release files
+if [ -z "$FEDORA$SUSE$UBUNTU" ]; then
+	if   [ -f /etc/redhat-release ]; then FEDORA=1; USERS="sudo"; 
+	elif [ -f /etc/fedora-release ]; then FEDORA=1; USERS="sudo"; 
+	elif [ -f /etc/SuSE-release ];   then SUSE=1; USERS="su";
+	elif [ -f /etc/debian_version ]; then UBUNTU=1; USERS="su -c";
+	fi
+fi
 make clean
 #rm .deps/*enigma2*
 make yaud-enigma2-nightly
@@ -15,10 +40,10 @@ cd $PINGUDIR/tufsbox
 if [ ! -e $EVOLUXDIR ]; then
 mkdir -p $EVOLUXDIR
 fi
-sudo rm -rf $EVOLUXDIR/*
+$USERS rm -rf $EVOLUXDIR/*
 cd $NEUTRINODIR
-sudo tar -czvf $PINGUDIR/tufsbox/release_neutrino_with_dev_for_EVOLUX.tar.gz ./
-sudo tar -xzvf $PINGUDIR/tufsbox/release_neutrino_with_dev_for_EVOLUX.tar.gz -C $EVOLUXDIR/
+$USERS tar -czvf $PINGUDIR/tufsbox/release_neutrino_with_dev_for_EVOLUX.tar.gz ./
+$USERS  tar -xzvf $PINGUDIR/tufsbox/release_neutrino_with_dev_for_EVOLUX.tar.gz -C $EVOLUXDIR/
 cp -RP $E2DIR/var $EVOLUXDIR/
 cp -RP $E2DIR/sbin $EVOLUXDIR/
 cp -RP $E2DIR/usr/bin $EVOLUXDIR/usr/
@@ -44,11 +69,11 @@ cp -RP $E2DIR/etc/rc.d/rc0.d $EVOLUXDIR/etc/rc.d/
 cp -RP $E2DIR/etc/init.d/ntpupdate.sh $EVOLUXDIR/etc/init.d/
 cp -RP $CURDIR/root/release/rcS_stm23_evolux_spark $EVOLUXDIR/etc/init.d/
 cd $EVOLUXDIR
-sudo tar -czvf../release_EVOLUX_with_dev.tar.gz ./
+$USERS  tar -czvf../release_EVOLUX_with_dev.tar.gz ./
 cp -RP ../release_EVOLUX_with_dev.tar.gz $DESTINATION/
-sudo rm -rf $DESTINATION/enigma2/*
+$USERS  rm -rf $DESTINATION/enigma2/*
 cd $DESTINATION && sudo tar -xzvf release_EVOLUX_with_dev.tar.gz -C $DESTINATION/enigma2/
-sudo chmod 777 -R $DESTINATION/enigma2
+$USERS  chmod 777 -R $DESTINATION/enigma2
 cp -RP $EVOLUXDIR/boot/uImage $PINGUDIR/tufsbox/
 myVERSION=`cat $EVOLUXDIR/etc/changelog.txt | grep -m1 Version | cut -d = -f2`
 $PINGUDIR/tufsbox/host/bin/mkfs.jffs2 -r $EVOLUXDIR -o $PINGUDIR/tufsbox/e2jffs2.img -e 0x20000 -n
