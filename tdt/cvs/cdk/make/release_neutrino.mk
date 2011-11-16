bb36f19977ac3d2d746cffab56a13de3073dffa3
~pinky1981 <al_borland33@yahoo.de>
2011-11-17 00:41:50 +0100

    neutrino update

--- tdt/cvs/cdk/make/release_neutrino.mk
+++ tdt/cvs/cdk/make/release_neutrino.mk
@@ -432,6 +432,14 @@ endif
 	find $(prefix)/release_neutrino/usr/lib/ -name '*.so*' -exec sh4-linux-strip --strip-unneeded {} \;
 
 	cp -f $(buildprefix)/root/etc/init.d/Swap.sh $(prefix)/release_neutrino/etc/init.d/
+#	install autofs
+	cp -f $(targetprefix)/usr/sbin/automount $(prefix)/release/usr/sbin/
+	cp $(targetprefix)/lib/modules/$(KERNELVERSION)/kernel/fs/autofs4/autofs4.ko $(prefix)/release_neutrino/lib/modules
+	cp -f $(buildprefix)/root/release/auto.usb $(prefix)/release_neutrino/etc/
+	echo 'sda    -fstype=auto,noatime,nodiratime          :/dev/sda' >> $(prefix)/release_neutrino/etc/auto.usb
+	echo 'sda1   -fstype=auto,noatime,nodiratime          :/dev/sda1' >> $(prefix)/release_neutrino/etc/auto.usb
+	echo 'sda2   -fstype=auto,noatime,nodiratime          :/dev/sda2' >> $(prefix)/release_neutrino/etc/auto.usb
+	echo 'sda3   -fstype=auto,noatime,nodiratime          :/dev/sda3' >> $(prefix)/release_neutrino/etc/auto.usb
 #
 ######## FOR YOUR OWN CHANGES use these folder in cdk/own_build/neutrino #############
 #	rm $(prefix)/release_neutrino/bin/mount
