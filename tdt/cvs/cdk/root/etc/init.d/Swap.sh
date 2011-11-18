01beb6bdada3ea34fe440ee18b90e0742bf0cf35
~pinky1981 <al_borland33@yahoo.de>
2011-11-18 22:31:06 +0100

    swap fix

--- tdt/cvs/cdk/root/etc/init.d/Swap.sh
+++ tdt/cvs/cdk/root/etc/init.d/Swap.sh
@@ -121,107 +121,108 @@ if [ -e /etc/.start_enigma2 ]; then
 			fi
 		fi
 else
-if [ "$isactive2" = swappart ] || [ "$isactive2" = swapfile ] || [ "$isactive2" = swapram ]; then
-		if [ "$isactive2" = swappart ]; then
-			swappart=`fdisk -l | grep swap | cut -d / -f3 | cut -b1-4`
-			echo -e -n "\n/dev/$swappart     none                swap    sw\n" >> /etc/fstab
-			swapon -a
-			echo "SWAPPART ON"
-			echo "SWP" > /dev/vfd
-		fi
-		if [ "$isactive2" = swapram ]; then
-	#		if [ -e /lib/modules/lzo1x_decompress.ko ]; then
-				alwaysthere=`lsmod | grep -m2 ramzswap0`
-				if [ -z $alwaysthere ]; then
-					isSTM24=`uname -a | grep stm23`
-					if [ -z $isSTM24 ]; then
-						insmod /lib/modules/lzo1x_compress.ko
-						insmod /lib/modules/lzo1x_decompress.ko
-					fi
-					insmod /lib/modules/ramzswap.ko disksize_kb=32768
-					mknod /dev/ramzswap0 b 253 0 >/dev/null 2>&1
-					echo "SWAPRAM ON"
-					echo "SWR" > /dev/vfd
-					loopswap=`cat /proc/swaps | grep loop -m1 | cut -d " " -f1`
-					if [ ! -z $loopswap ];then
-						swapoff -a
-						swapoff $loopswap
-						swapon /dev/ramzswap0
-					else
-						(swapoff -a;swapon /dev/ramzswap0)
-					fi 
-				fi
-	#		fi
-		fi
-		if [ "$isactive2" = swapfile ]; then
-			. /etc/init.d/swapsize.old
-			size=`cat /etc/enigma2/settings | grep config.plugins.PinkPanel.SwapFileSize= | cut -d = -f2`
-			if [ -z "$size" ]; then
-				size=16384
+	if [ "$isactive2" = swappart ] || [ "$isactive2" = swapfile ] || [ "$isactive2" = swapram ]; then
+			if [ "$isactive2" = swappart ]; then
+				swappart=`fdisk -l | grep swap | cut -d / -f3 | cut -b1-4`
+				echo -e -n "\n/dev/$swappart     none                swap    sw\n" >> /etc/fstab
+				swapon -a
+				echo "SWAPPART ON"
+				echo "SWP" > /dev/vfd
 			fi
-			isHDD=`mount | grep sda`
-			if [ ! -z "$isHDD" ]; then
-				if [ ! -e /media/hdd/swap ]; then
-					mkdir -p /media/hdd/swap
+			if [ "$isactive2" = swapram ]; then
+		#		if [ -e /lib/modules/lzo1x_decompress.ko ]; then
+					alwaysthere=`lsmod | grep -m2 ramzswap0`
+					if [ -z $alwaysthere ]; then
+						isSTM24=`uname -a | grep stm23`
+						if [ -z $isSTM24 ]; then
+							insmod /lib/modules/lzo1x_compress.ko
+							insmod /lib/modules/lzo1x_decompress.ko
+						fi
+						insmod /lib/modules/ramzswap.ko disksize_kb=32768
+						mknod /dev/ramzswap0 b 253 0 >/dev/null 2>&1
+						echo "SWAPRAM ON"
+						echo "SWR" > /dev/vfd
+						loopswap=`cat /proc/swaps | grep loop -m1 | cut -d " " -f1`
+						if [ ! -z $loopswap ];then
+							swapoff -a
+							swapoff $loopswap
+							swapon /dev/ramzswap0
+						else
+							(swapoff -a;swapon /dev/ramzswap0)
+						fi 
+					fi
+		#		fi
+			fi
+			if [ "$isactive2" = swapfile ]; then
+				. /etc/init.d/swapsize.old
+				size=`cat /etc/enigma2/settings | grep config.plugins.PinkPanel.SwapFileSize= | cut -d = -f2`
+				if [ -z "$size" ]; then
+					size=16384
 				fi
-				if [ ! -e /media/hdd/swap/swapfile ] && [ "$isactive2" = swapfile ]; then
-					echo "SWAP create $(($size / 1000)) MB swapfile"
-					echo "CRS" > /dev/vfd
-					loopswapramold=`cat /proc/swaps | grep ram -m1 | cut -d " " -f1`
-					loopswapold=`cat /proc/swaps | grep loop -m1 | cut -d " " -f1`
-					swapoff -a
-					if [ ! -z $loopswapramold ]; then
-						swapoff $loopswapramold
+				isHDD=`mount | grep sda`
+				if [ ! -z "$isHDD" ]; then
+					if [ ! -e /media/hdd/swap ]; then
+						mkdir -p /media/hdd/swap
 					fi
-					if [ ! -z $loopswapold ]; then
-						swapoff $loopswapold
-					fi
-					swapoff /dev/loop0
-					dd if=/dev/zero of=/media/hdd/swap/swapfile bs=1024 count=$size
-					losetup /dev/loop0 /media/hdd/swap/swapfile
-					mkswap /dev/loop0
-					swapon /dev/loop0
-					sync
-				elif [ "$swapsize" != "$size" ] && [ "$isactive2" = swapfile ]; then
-					echo "SWAP create NEW $(($size / 1000)) MB swapfile"
-					echo "CRS" > /dev/vfd
-					loopswap=`cat /proc/swaps | cut -b10`
-					swapoff -a
-					rm /media/hdd/swap/swapfile
-					dd if=/dev/zero of=/media/hdd/swap/swapfile bs=1024 count=$size
-					echo "swapsize=$size" > /etc/init.d/swapsize.old
-					loopswapnew=$(($loopswap + 1))
-					losetup /dev/loop$loopswapnew /media/hdd/swap/swapfile
-					mkswap /dev/loop$loopswapnew
-					loopswapold=`cat /proc/swaps | grep loop -m1 | cut -d " " -f1`
-					loopswapramold=`cat /proc/swaps | grep ram -m1 | cut -d " " -f1`
-					swapoff $loopswapold
-					if [ ! -z $loopswapramold ]; then
-						swapoff $loopswapramold
-					fi
-					swapon /dev/loop$loopswapnew
-					sync
-				elif [ -e /media/hdd/swap/swapfile ] && [ "$swapsize" = "$size" ] && [ "$isactive2" = swapfile ]; then
-					loopswap=`cat /proc/swaps | grep loop -m1 | cut -d " " -f1`
-					echo "ACTIVATING SWAPFILE"
-					echo "SWF" > /dev/vfd
-					if [ ! -z $loopswap ]; then
+					if [ ! -e /media/hdd/swap/swapfile ] && [ "$isactive2" = swapfile ]; then
+						echo "SWAP create $(($size / 1000)) MB swapfile"
+						echo "CRS" > /dev/vfd
+						loopswapramold=`cat /proc/swaps | grep ram -m1 | cut -d " " -f1`
+						loopswapold=`cat /proc/swaps | grep loop -m1 | cut -d " " -f1`
 						swapoff -a
-						swapoff $loopswap
-						losetup $loopswap /media/hdd/swap/swapfile
-						mkswap $loopswap
-						swapon $loopswap
-					else
-						swapoff -a
+						if [ ! -z $loopswapramold ]; then
+							swapoff $loopswapramold
+						fi
+						if [ ! -z $loopswapold ]; then
+							swapoff $loopswapold
+						fi
 						swapoff /dev/loop0
+						dd if=/dev/zero of=/media/hdd/swap/swapfile bs=1024 count=$size
 						losetup /dev/loop0 /media/hdd/swap/swapfile
 						mkswap /dev/loop0
 						swapon /dev/loop0
+						sync
+					elif [ "$swapsize" != "$size" ] && [ "$isactive2" = swapfile ]; then
+						echo "SWAP create NEW $(($size / 1000)) MB swapfile"
+						echo "CRS" > /dev/vfd
+						loopswap=`cat /proc/swaps | cut -b10`
+						swapoff -a
+						rm /media/hdd/swap/swapfile
+						dd if=/dev/zero of=/media/hdd/swap/swapfile bs=1024 count=$size
+						echo "swapsize=$size" > /etc/init.d/swapsize.old
+						loopswapnew=$(($loopswap + 1))
+						losetup /dev/loop$loopswapnew /media/hdd/swap/swapfile
+						mkswap /dev/loop$loopswapnew
+						loopswapold=`cat /proc/swaps | grep loop -m1 | cut -d " " -f1`
+						loopswapramold=`cat /proc/swaps | grep ram -m1 | cut -d " " -f1`
+						swapoff $loopswapold
+						if [ ! -z $loopswapramold ]; then
+							swapoff $loopswapramold
+						fi
+						swapon /dev/loop$loopswapnew
+						sync
+					elif [ -e /media/hdd/swap/swapfile ] && [ "$swapsize" = "$size" ] && [ "$isactive2" = swapfile ]; then
+						loopswap=`cat /proc/swaps | grep loop -m1 | cut -d " " -f1`
+						echo "ACTIVATING SWAPFILE"
+						echo "SWF" > /dev/vfd
+						if [ ! -z $loopswap ]; then
+							swapoff -a
+							swapoff $loopswap
+							losetup $loopswap /media/hdd/swap/swapfile
+							mkswap $loopswap
+							swapon $loopswap
+						else
+							swapoff -a
+							swapoff /dev/loop0
+							losetup /dev/loop0 /media/hdd/swap/swapfile
+							mkswap /dev/loop0
+							swapon /dev/loop0
+						fi
 					fi
 				fi
 			fi
 		fi
-fi
+	fi
 	swapon -a
 fi
 exit
