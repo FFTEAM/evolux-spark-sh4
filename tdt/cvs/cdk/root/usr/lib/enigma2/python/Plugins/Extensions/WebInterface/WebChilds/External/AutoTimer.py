a599fb8ef88e2c471bdfc7587bb2f0f817c40b7b
~pinky1981 <al_borland33@yahoo.de>
2011-11-26 11:02:29 +0100

    cleanup

--- tdt/cvs/cdk/root/usr/lib/enigma2/python/Plugins/Extensions/WebInterface/WebChilds/External/AutoTimer.py
+++
@@ -1,8 +0,0 @@
-from Plugins.Extensions.WebInterface.WebChilds.Toplevel import addExternalChild
-from Plugins.Extensions.AutoTimer.AutoTimerResource import AutoTimerDoParseResource, \
-	AutoTimerListAutoTimerResource
-
-root = AutoTimerListAutoTimerResource()
-root.putChild('parse', AutoTimerDoParseResource())
-addExternalChild( ("autotimer", root ) )
-
--- tdt/cvs/cdk/root/usr/lib/enigma2/python/Plugins/Extensions/WebInterface/WebChilds/External/BarryAllen.py
+++
@@ -1,6 +0,0 @@
-from Plugins.Extensions.WebInterface.WebChilds.Toplevel import addExternalChild
-import os
-if os.path.exists('/media/ba/BarryAllen/BarryAllen.py') == True:
-   from Plugins.Extensions.BarryAllen.BarryAllen import BarryAllen
-   addExternalChild( ("barryallen", BarryAllen()) )
-
