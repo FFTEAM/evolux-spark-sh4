266998935543918cfaeeb0c3f2b43f9a7799a97b
~pinky1981 <al_borland33@yahoo.de>
2011-12-09 17:39:50 +0100

    flash update, many thx to ahpook5a

--- tdt/flash/spark/flash_E2_yaffs2.sh
+++ tdt/flash/spark/flash_E2_yaffs2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-### /storage/c/enigma2/flash_E2_yaffs2.sh ###
-if [ -e /storage/c/enigma2/fw_env.config ]; then
-cp -RP /storage/c/enigma2/fw_env.config /etc/
+### /storage/c/enigma2/flash_E2_yaffs2.sh ####
+if [ -e /storage/c/enigma2/etc/fw_env.config ]; then
+cp -RP /storage/c/enigma2/etc/fw_env.config /etc/
 chmod 755 /etc/fw_env.config
 fi
 
@@ -24,38 +24,58 @@ esac
 
 case "$REPLY" in
 1)
-if [ -e /storage/c/enigma2/e2yaffs2.img ] && [ -e /storage/c/enigma2/fw_env.config ]; then
+if [ -e /storage/c/enigma2/e2yaffs2.img ] && [ -e /storage/c/enigma2/etc/fw_env.config ]; then
+read -p "are you sure? start flashing YAFFS2 now (Y/n) ? "
+if [ "$REPLY" == "Y" ] || ["$REPLY" == "y" ]; then
 flash_eraseall /dev/mtd5
 nandwrite -a -p -m /dev/mtd5 /storage/c/enigma2/uImage
 flash_eraseall /dev/mtd6
 nandwrite -a -o /dev/mtd6 /storage/c/enigma2/e2yaffs2.img
-/storage/c/enigma2/setmtdmode
-/storage/c/enigma2/fw_setenv -s /storage/c/enigma2/bootargs_evolux_yaffs2
-/storage/c/enigma2/setmtdmode -l
-/storage/c/enigma2/fw_printenv
-echo "Done! Bootargs also set to Evolux-YAFFS2!"
-echo "We reboot in 10s and Evolux should start with neutrino..."
+else
+exit
+fi
+read -p "are you sure? change bootargs now to YAFFS2 (Y/n) ? "
+if [ "$REPLY" == "Y" ] || ["$REPLY" == "y" ]; then
+/storage/c/enigma2/bin/setmtdmode
+/storage/c/enigma2/bin/fw_setenv -s /storage/c/enigma2/etc/bootargs_evolux_yaffs2
+/storage/c/enigma2/bin/setmtdmode -l
+/storage/c/enigma2/bin/fw_printenv
+echo "All Done! Bootargs also set to YAFFS2!"
+echo "We reboot in 10s and Evolux should booting with neutrino..."
 sleep 10
 reboot -f
+else
+exit
+fi
 else
 echo "ERROR! Not all files found on stick!"
 exit
 fi
 ;;
 2)
-if [ -e /storage/c/enigma2/e2jffs2.img ] && [ -e /storage/c/enigma2/fw_env.config ]; then
+if [ -e /storage/c/enigma2/e2jffs2.img ] && [ -e /storage/c/enigma2/etc/fw_env.config ]; then
+read -p "are you sure? start flashing YAFFS2 now (Y/n) ? "
+if [ "$REPLY" == "Y" ] || ["$REPLY" == "y" ]; then
 flash_eraseall /dev/mtd5
 nandwrite -a -p -m /dev/mtd5 /storage/c/enigma2/uImage
 flash_eraseall /dev/mtd6
 nandwrite -a -m -p /dev/mtd6 /storage/c/enigma2/e2jffs2.img
-/storage/c/enigma2/setmtdmode
-/storage/c/enigma2/fw_setenv -s /storage/c/enigma2/bootargs_evolux_jffs2
-/storage/c/enigma2/setmtdmode -l
-/storage/c/enigma2/fw_printenv
-echo "Done! Bootargs also set to Evolux-JFFS2!"
-echo "We reboot in 10s and Evolux should start with neutrino..."
+else
+exit
+fi
+read -p "are you sure? change bootargs now to YAFFS2 (Y/n) ? "
+if [ "$REPLY" == "Y" ] || ["$REPLY" == "y" ]; then
+/storage/c/enigma2/bin/setmtdmode
+/storage/c/enigma2/bin/fw_setenv -s /storage/c/enigma2/etc/bootargs_evolux_jffs2
+/storage/c/enigma2/bin/setmtdmode -l
+/storage/c/enigma2/bin/fw_printenv
+echo "All Done! Bootargs also set to JFFS2!"
+echo "We reboot in 10s and your e2jjfs2.img should booting..."
 sleep 10
 reboot -f
+else
+exit
+fi
 else
 echo "ERROR! Not all files found on stick!"
 exit
