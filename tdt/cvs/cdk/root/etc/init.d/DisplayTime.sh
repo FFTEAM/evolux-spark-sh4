446e74cc8fdb4550f0919414d3bea6349f4ba765
~pinky1981 <al_borland33@yahoo.de>
2011-12-29 23:03:41 +0100

    neutrino: DisplayTime.sh optimized, thx ahpokk5a

--- tdt/cvs/cdk/root/etc/init.d/DisplayTime.sh
+++ tdt/cvs/cdk/root/etc/init.d/DisplayTime.sh
@@ -1,26 +1,10 @@
 #!/bin/sh
 export LD_LIBRARY_PATH=/lib:$LD_LIBRARY_PATH
-
-tel=1
 export TZ=MEZ-1MESZ,M3.5.0/2,M10.5.0/3
-while [ 1 -eq 1 ]; do
-    Stime=`date "+%H%M`
-
-    # Update display when new minute
-    if [ "$Stime" != "$Oldtime" ]; then
-#        /bin/fp_control -t "     $Stime"
-	echo "$Stime" > /dev/vfd
-        Oldtime=$Stime
-    fi
-
-    # Update display each 30 sec.
-    if [ $tel -gt 30 ]; then
-#        /bin/fp_control -t "     $Stime"
-	echo "$Stime" > /dev/vfd
-        tel=1
-    fi
-
-    tel=`expr $tel + 1`
-    sleep 1
+while [ 1 ]
+do
+        set `date "+%H %M %S"`
+        echo $1$2 >/dev/vfd
+        sleep $((60 - $3))
 done
 exit
