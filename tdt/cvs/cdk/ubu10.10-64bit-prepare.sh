96f3836415bd95510e1eb9414043590116e6ab2d
~pinky1981 <al_borland33@yahoo.de>
2011-12-30 12:22:28 +0100

    ubu10.10-64bit build update

--- tdt/cvs/cdk/ubu10.10-64bit-prepare.sh
+++ tdt/cvs/cdk/ubu10.10-64bit-prepare.sh
@@ -1,17 +1,72 @@
 #!/bin/bash
 CURDIR=`pwd`
-if [ ! -e "$CURDIR/.ubu1010" ]; then
-	echo "deb http://old-releases.ubuntu.com/ubuntu/ jaunty main restricted" > /etc/apt/sources.list
-	echo "deb-src http://old-releases.ubuntu.com/ubuntu/ jaunty main" > /etc/apt/sources.list
-	sudo apt-get update
-	sudo apt-get remove rpm
-	sudo apt-get -y install rpm=4.4.2.3-2ubuntu1
-	echo "please lock new/old rpm verison now by packetmanger!!!"
-	sleep 5
-	sudo ln -sf /bin/bash /bin/sh
-	cd /usr/lib32
-	sudo ln -sf n -s libz.so.1.2.3.4 libz.so
-	touch "$CURDIR/.ubu1010"
-fi
+echo "deb http://old-releases.ubuntu.com/ubuntu/ jaunty main restricted" > /etc/apt/sources.list
+echo "deb-src http://old-releases.ubuntu.com/ubuntu/ jaunty main" > /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get remove rpm
+sudo apt-get -y install rpm=4.4.2.3-2ubuntu1
+echo "please lock new/old rpm version now by packetmanger!!!"
+sleep 5
+sudo ln -sf /bin/bash /bin/sh
+#	cd /usr/lib32
+#	sudo ln -sf n -s libz.so.1.2.3.4 libz.so
+#	touch "$CURDIR/.ubu1010"
+apt-get -y install subversion \
+git-core \
+ccache \
+automake \
+libncurses5-dev \
+flex \
+bison \
+gawk \
+texinfo \
+gettext \
+cfv \
+fakeroot \
+xfslibs-dev \
+zlib1g-dev \
+libtool \
+g++ \
+gcc-multilib \
+swig \
+pkg-config \
+ndisgtk \
+git-core \
+mtd-utils \
+squashfs-tools \
+help2man \
+diffstat \
+texi2html \
+bitbake \
+monotone \
+rar \
+unrar \
+ffmpeg \
+sysfsutils \
+libdvdcss2 \
+libssl-dev \
+libbz2-dev \
+libgtk2.0-dev \
+padevchooser \
+ubuntu-restricted-extras \
+jam \
+libgif-dev \
+imagemagick \
+optipng \
+pngquant \
+gsynaptics \
+ghex \
+gparted \
+ntfsprogs \
+libmpc-dev \
+libgmp3-dev \
+libmpfr-dev \
+g++-multilib \
+g++-4.2-multilib \
+lib32z1-dev \
+autopoint \
+ntfs-config \
+lib32z1-dev
+
 exit
 
