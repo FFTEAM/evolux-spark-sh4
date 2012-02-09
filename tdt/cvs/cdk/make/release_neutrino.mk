47023755d055f2a816e28b407f500d29c1c2631a
~pinky1981 <al_borland33@yahoo.de>
2012-02-09 18:04:02 +0100

    del old blcokad

--- tdt/cvs/cdk/make/release_neutrino.mk
+++ tdt/cvs/cdk/make/release_neutrino.mk
@@ -524,6 +524,8 @@ endif
 	cp -RP $(targetprefix)/usr/var/tuxbox/config/* $(prefix)/release_neutrino/usr/local/share/config/
 #	cp -RP $(targetprefix)/usr/bin/blockad $(prefix)/release_neutrino/bin/
 	rm $(prefix)/release_neutrino/var/plugins/blockad*
+	rm $(prefix)/release_neutrino/usr/lib/tuxbox/plugins/blockad*
+	rm $(prefix)/release_neutrino/usr/local/share/config/blockad*
 	touch $(prefix)/release_neutrino/etc/.EVOfirstboot
 
 	[ -e $(kernelprefix)/$(kernelpath)/drivers/usb/serial/ftdi_sio.ko ] && cp $(kernelprefix)/$(kernelpath)/drivers/usb/serial/ftdi_sio.ko $(prefix)/release_neutrino/lib/modules/ftdi.ko || true
