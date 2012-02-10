d837312f6ed4da56c0a62c273d6bdd175a29a9fe
~pinky1981 <al_borland33@yahoo.de>
2012-02-10 16:54:51 +0100

    readd player2 patch

--- tdt/cvs/driver/player2_191/player/manifestor/pcmplayer_ksound.cpp
+++ tdt/cvs/driver/player2_191/player/manifestor/pcmplayer_ksound.cpp
@@ -211,7 +211,8 @@ PlayerStatus_t PcmPlayer_Ksound_c::MapSamples(unsigned int SampleCount, bool Non
 	        	            -Avail, Identity);
 	            Status = DeployUnderrunRecovery();
 	            if (PlayerNoError != Status)
-	                return Status;
+//			return Status;
+			return PlayerNoError;
 	        }
 	        
 		if (NonBlock) {
@@ -351,7 +352,8 @@ PlayerStatus_t PcmPlayer_Ksound_c::SetParameters(PcmPlayerSurfaceParameters_t *P
                 // this is not a 'hard' error hence this message is neither _TRACEd nor _ERRORed.
                 // our caller knows that if we return an error it has to wait until the samples are unmapped...
                 PCMPLAYER_DEBUG("Not safe to update soundcard parameters for %s at this point\n", Identity);
-    		return PlayerError;
+//		return PlayerError;
+		return PlayerNoError;
 	}
 	
 	// calculate the resampling factor
