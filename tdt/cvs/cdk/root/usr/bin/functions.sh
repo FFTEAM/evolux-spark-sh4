79230805291f5dfd8a1865bdcec9688e5e0f1e7f
~pinky1981 <al_borland33@yahoo.de>
2012-02-17 11:43:41 +0100

    lircrc activated, thx martii

--- tdt/cvs/cdk/root/usr/bin/functions.sh
+++ tdt/cvs/cdk/root/usr/bin/functions.sh
@@ -4,9 +4,11 @@ case $1 in
 	ismounted=`mount | grep sda1`
 	if [ ! -z "$ismounted" ]; then
 		umount /dev/sda1
+		wget -O /dev/null http://127.0.0.1/control/message?popup="USB unmounted, now you can disconnect hdd/stick" &
 	else
 		mount /dev/sda1 /autofs/sda1
 		mount -o bind /autofs/sda1 /media/hdd
+		wget -O /dev/null http://127.0.0.1/control/message?popup="USB (re)mounted, now you can use hdd/stick" &
 esac
 exit
 
