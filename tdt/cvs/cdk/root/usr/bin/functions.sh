b683e4a872112d9c34ec102afa83fd34e3f64f69
~pinky1981 <al_borland33@yahoo.de>
2012-02-17 11:53:13 +0100

    lircrc activated, thx martii

--- tdt/cvs/cdk/root/usr/bin/functions.sh
+++ tdt/cvs/cdk/root/usr/bin/functions.sh
@@ -1,14 +1,27 @@
 #!/bin/sh
 case $1 in
 0)
-	ismounted=`mount | grep sda1`
-	if [ ! -z "$ismounted" ]; then
-		umount /dev/sda1
-		wget -O /dev/null http://127.0.0.1/control/message?popup="USB unmounted, now you can disconnect hdd/stick" &
-	else
-		mount /dev/sda1 /autofs/sda1
-		mount -o bind /autofs/sda1 /media/hdd
-		wget -O /dev/null http://127.0.0.1/control/message?popup="USB (re)mounted, now you can use hdd/stick" &
+	if [ ! -e /etc/.start_enigma2 ]; then
+		ismounted=`mount | grep sda1`
+		if [ ! -z "$ismounted" ]; then
+			umount /dev/sda1
+			umounted=`mount | grep sda1`
+			if [ -z "$umounted" ]; then
+				wget -O /dev/null http://127.0.0.1/control/message?popup="USB unmounted, now you can disconnect hdd/stick" &
+			else
+				wget -O /dev/null http://127.0.0.1/control/message?popup="ERROR" &
+			fi
+		else
+			mount /dev/sda1 /autofs/sda1
+			mount -o bind /autofs/sda1 /media/hdd
+			remounted=`mount | grep sda1`
+			if [ ! -z "$remounted" ]; then
+				wget -O /dev/null http://127.0.0.1/control/message?popup="USB (re)mounted, now you can use hdd/stick" &
+			else
+				wget -O /dev/null http://127.0.0.1/control/message?popup="ERROR" &
+			fi
+		fi
+	fi
 ;;
 
 *)
