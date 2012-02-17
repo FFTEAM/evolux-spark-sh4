ba4b27e5da7feb895a3848ffe46e4186b0e3436f
~pinky1981 <al_borland33@yahoo.de>
2012-02-17 11:44:42 +0100

    lircrc activated, thx martii

--- tdt/cvs/cdk/root/usr/bin/functions.sh
+++ tdt/cvs/cdk/root/usr/bin/functions.sh
@@ -9,6 +9,11 @@ case $1 in
 		mount /dev/sda1 /autofs/sda1
 		mount -o bind /autofs/sda1 /media/hdd
 		wget -O /dev/null http://127.0.0.1/control/message?popup="USB (re)mounted, now you can use hdd/stick" &
+;;
+
+*)
+	exit
+;;
 esac
 exit
 
