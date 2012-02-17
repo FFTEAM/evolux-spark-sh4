bce0c2f2d3d9af69f13cb44441488b4013678038
~pinky1981 <al_borland33@yahoo.de>
2012-02-17 12:12:48 +0100

    lircrc activated, thx martii

--- tdt/cvs/cdk/root/usr/bin/functions.sh
+++ tdt/cvs/cdk/root/usr/bin/functions.sh
@@ -21,6 +21,26 @@ case $1 in
 				wget -O /dev/null http://127.0.0.1/control/message?popup="ERROR" &
 			fi
 		fi
+	else
+		ismounted=`mount | grep sda1`
+		if [ ! -z "$ismounted" ]; then
+			umount /dev/sda1
+			umounted=`mount | grep sda1`
+			if [ -z "$umounted" ]; then
+				wget "http://127.0.0.1/web/message?text=USB%20unmounted,%20now%20you%20can%20disconnect%20hdd/stick&type=1&timeout=5" &
+			else
+				wget "http://127.0.0.1/web/message?text=ERROR&type=3&timeout=5" &
+			fi
+		else
+			mount /dev/sda1 /autofs/sda1
+			mount -o bind /autofs/sda1 /media/hdd
+			remounted=`mount | grep sda1`
+			if [ ! -z "$remounted" ]; then
+				wget "http://127.0.0.1/web/message?text=USB%20(re)mounted,%20now%20you%20can%20use%20hdd/stick&type=1&timeout=5" &
+			else
+				wget "http://127.0.0.1/web/message?text=ERROR&type=3&timeout=5" &
+			fi
+		fi
 	fi
 ;;
 
