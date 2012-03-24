1db899aae743ea5a17bc64e54c5265de4b9dda8e
~pinky1981 <al_borland33@yahoo.de>
2012-03-24 14:42:44 +0100

    scart patch as option stmcoredisplay set, thx martii

--- tdt/cvs/cdk/make/release.mk
+++ tdt/cvs/cdk/make/release.mk
@@ -91,7 +91,7 @@ if ENABLE_P0209
 	cp $(targetprefix)/lib/modules/$(KERNELVERSION)/extra/wireless/rtl8192cu/8192cu.ko $(prefix)/release/lib/modules/rt8192cu.ko
 #	cp $(targetprefix)/lib/modules/$(KERNELVERSION)/extra/wireless/rtl871x/8712u.ko $(prefix)/release/lib/modules/rt8712u.ko
 	cp -f $(buildprefix)/root/lib/modules/rt8712u_stm24-209.ko $(prefix)/release/lib/modules/rt8712u.ko
-	cp -f $(buildprefix)/root/lib/modules/stmcore-display-sti7111_stm24-209_scart.ko $(prefix)/release/lib/modules/stmcore-display-sti7111_scart.ko
+#	cp -f $(buildprefix)/root/lib/modules/stmcore-display-sti7111_stm24-209_scart.ko $(prefix)/release/lib/modules/stmcore-display-sti7111_scart.ko
 	cp $(targetprefix)/lib/modules/$(KERNELVERSION)/extra/wireless/rt2870sta/rt2870sta.ko $(prefix)/release/lib/modules/
 	cp $(targetprefix)/lib/modules/$(KERNELVERSION)/extra/wireless/rt3070sta/rt3070sta.ko $(prefix)/release/lib/modules/
 	cp $(targetprefix)/lib/modules/$(KERNELVERSION)/extra/wireless/rt5370sta/rt5370sta.ko $(prefix)/release/lib/modules/
