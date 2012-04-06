c36490b8db0f136038366aad57d2b14f93751c19
~pinky1981 <al_borland33@yahoo.de>
2012-04-06 18:11:09 +0200

    fix

--- tdt/cvs/cdk/make/neutrino.mk
+++ tdt/cvs/cdk/make/neutrino.mk
@@ -13,7 +13,7 @@ $(targetprefix)/var/etc/.version:
 #NORMAL
 #
 
-$(appsdir)/neutrino/config.status: bootstrap freetype libpng libid3tag openssl curl libmad libboost libgif jpeg sdparm $(EXTERNALLCD_DEP)
+$(appsdir)/neutrino/config.status: bootstrap freetype libpng libid3tag openssl curl libmad libboost libgif jpeg sdparm graphlcd
 	export PATH=$(hostprefix)/bin:$(PATH) && \
 	cd $(appsdir)/neutrino && \
 		ACLOCAL_FLAGS="-I $(hostprefix)/share/aclocal" ./autogen.sh && \
