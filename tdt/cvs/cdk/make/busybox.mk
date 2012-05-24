96eb183bda612bad5fb99e032646404b172bb720
~pinky1981 <al_borland33@yahoo.de>
2012-05-24 16:17:24 +0200

    busybox update to 1.20.0, thx bpanther

--- tdt/cvs/cdk/make/busybox.mk
+++ tdt/cvs/cdk/make/busybox.mk
@@ -4,14 +4,14 @@
 $(DEPDIR)/busybox.do_prepare: @DEPENDS_busybox@
 	@PREPARE_busybox@
 	cd @DIR_busybox@
-#	cd @DIR_busybox@ && \
-#		patch -p1 < ../Patches/busybox-1.19.3/busybox-1.19.3-getty.patch && \
-#		patch -p1 < ../Patches/busybox-1.19.3/busybox-1.19.3-mdev.patch && \
-#		patch -p1 < ../Patches/busybox-1.19.3/busybox-1.19.3-modinfo.patch && \
-#		patch -p1 < ../Patches/busybox-1.19.3/busybox-1.19.3-wget.patch
+	cd @DIR_busybox@ && \
+		patch -p1 < ../Patches/busybox-1.20.0/busybox-1.20.0-buildsys.patch && \
+		patch -p1 < ../Patches/busybox-1.20.0/busybox-1.20.0-getty.patch && \
+		patch -p1 < ../Patches/busybox-1.20.0/busybox-1.20.0-lineedit.patch && \
+		patch -p1 < ../Patches/busybox-1.20.0/busybox-1.20.0-sed.patch
 	touch $@
 
-$(DEPDIR)/busybox.do_compile: bootstrap $(DEPDIR)/busybox.do_prepare Patches/busybox-1.19.4/busybox-1.19.4.config | $(DEPDIR)/$(GLIBC_DEV)
+$(DEPDIR)/busybox.do_compile: bootstrap $(DEPDIR)/busybox.do_prepare Patches/busybox-1.20.0/busybox-1.20.0.config | $(DEPDIR)/$(GLIBC_DEV)
 	cd @DIR_busybox@ && \
 		export CROSS_COMPILE=$(target)- && \
 		$(MAKE) mrproper && \
@@ -30,17 +30,3 @@ $(DEPDIR)/%busybox: $(DEPDIR)/busybox.do_compile
 #		@CLEANUP_busybox@
 	@[ "x$*" = "x" ] && touch $@ || true
 	@TUXBOX_YAUD_CUSTOMIZE@
-
-flash-busybox: $(flashprefix)/root/bin/busybox
-
-$(flashprefix)/root/bin/busybox: $(DEPDIR)/busybox.do_compile
-	@cd @DIR_busybox@ && \
-		export CROSS_COMPILE=$(target)- && \
-		$(MAKE) install CONFIG_PREFIX=$(flashprefix)/root && \
-		export HHL_CROSS_TARGET_DIR=$(flashprefix)/root && $(hostprefix)/bin/target-shellconfig --add /bin/ash 5 && \
-		( cd $(flashprefix)/root && \
-			[ -f linuxrc ] && rm linuxrc || true ) && \
-	touch $@
-	@FLASHROOTDIR_MODIFIED@
-
-#.PHONY: flash-busybox
