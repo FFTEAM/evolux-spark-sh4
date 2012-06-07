3c099fa35cea268f29e8f099444b2fa1a0b5b45e
~pinky1981 <al_borland33@yahoo.de>
2012-06-07 18:07:22 +0200

    fix

--- tdt/custom/yaud-neutrino-hd-local.sh
+++ tdt/custom/yaud-neutrino-hd-local.sh
@@ -74,7 +74,6 @@ function make_devs() {
 		echo "Erstelle weitere /dev für SPARK" #$TARGET..."
 		echo "mknod -m 666 rc c 147 1" >> $CURDIR/.fakeroot
 		echo "mknod -m 666 sci0 c 169 0" >> $CURDIR/.fakeroot
-		echo "mknod -m 644 uinput c 10 223" >> $CURDIR/dev/input/
 		make_devs
 #	;;
 #	spark7162)
