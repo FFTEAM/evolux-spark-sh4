015546f2d7ea14fcdbecd15ad77e32cf261a52f5
martii <you@example.com>
2012-06-11 13:26:05 +0200

    neutrino hd/libstb-hal: change input handling to use a single file descriptor. Auxilliary program parts, like tuxtxt, can now be operated by any remote control (and HDMI-CEC, too).

--- tdt/cvs/apps/libstb-hal/libspark/init.cpp
+++ tdt/cvs/apps/libstb-hal/libspark/init.cpp
@@ -22,11 +22,43 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <linux/input.h>
+#include <time.h>
+#include <poll.h>
+#include <errno.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/prctl.h>
+#include <string.h>
+#include <signal.h>
 
-#define DEVINPUT "/dev/input"
 #define VIRTUALINPUT "/sys/devices/virtual/input"
+#define DEVINPUT "/dev/input"
 
-static int do_mknod(char *devname, char *i) {
+typedef struct {
+	const char *name;
+	const char *desc;
+	int fd;
+	time_t discovery;
+} input_device_t;
+
+static input_device_t input_device[] = {
+	{ "/dev/input/nevis_ir",	"lircd",			-1, 0 },
+	{ "/dev/input/tdt_rc",		"TDT RC event driver",		-1, 0 },
+	{ "/dev/input/fulan_fp",	"fulan front panel buttons",	-1, 0 },
+	{ "/dev/input/event0",		NULL,				-1, 0 },
+	{ "/dev/input/event1",		NULL,				-1, 0 },
+	{ "/dev/input/event2",		NULL,				-1, 0 },
+	{ "/dev/input/event3",		NULL,				-1, 0 },
+	{ "/dev/input/event4",		NULL,				-1, 0 },
+	{ "/dev/input/event5",		NULL,				-1, 0 },
+	{ "/dev/input/event6",		NULL,				-1, 0 },
+	{ "/dev/input/event7",		NULL,				-1, 0 },
+	{ NULL,				NULL,				-1, 0 }
+};
+
+static int number_of_input_devices = 0;
+
+static int do_mknod(const char *devname, char *i) {
 	char name[255];
 	int dev = -1;
 	// I've no idea how the event device number is actually calculated. Just loop.  --martii
@@ -44,15 +76,13 @@ static int do_mknod(char *devname, char *i) {
 			buf[l] = 0;
 			int major, minor;
 			if (2 == sscanf(buf, "%d:%d", &major, &minor)) {
-				snprintf(name, sizeof(name), DEVINPUT "/%s", devname);
-				unlink(name);
-				mknod(name, 0666 | S_IFCHR, makedev(major, minor));
+				mknod(devname, 0666 | S_IFCHR, makedev(major, minor));
 			}
 		}
 	}
 }
 
-void create_input_devices (void) {
+static void create_input_devices (void) {
 	DIR *d = opendir (VIRTUALINPUT);
 	if (d) {
 		struct dirent *e;
@@ -66,15 +96,16 @@ void create_input_devices (void) {
 				char buf[255];
 				int l = read(n, buf, sizeof(buf) - 1);
 				close(n);
-				if (l > -1) {
-					buf[l] = 0;
-					if (!strcmp(buf, "fulan front panel buttons\n"))
-						do_mknod("fulan_fp", e->d_name);
-					else if (!strcmp(buf, "TDT RC event driver\n"))
-						do_mknod("tdt_rc", e->d_name);
-					else if (!strcmp(buf, "lircd\n")){
-						do_mknod("nevis_ir", e->d_name);
-					}
+				if (l > 1) {
+					do
+						buf[l--] = 0;
+					while (l > 1 && buf[l] == '\n');
+
+					for (int i = 0; i < number_of_input_devices; i++)
+						if (input_device[i].desc && !strcmp(buf, input_device[i].desc)) {
+							do_mknod(input_device[i].name, e->d_name);
+							break;
+						}
 				}
 			}
 		}
@@ -96,17 +127,136 @@ void create_input_devices (void) {
 				char buf[255];
 				int l = ioctl(n, EVIOCGNAME(sizeof(buf)), buf);
 				close(n);
-				if (l > 0) {
-					buf[l] = 0;
-					if(!strcmp(buf, "fulan front panel buttons")
-					|| !strcmp(buf, "TDT RC event driver")
-					|| !strcmp(buf, "lircd"))
-						unlink(name);
+				if (l > 1) {
+					do
+						buf[l--] = 0;
+					while (l > 1 && buf[l] == '\n');
+
+					for (int i = 0; i < number_of_input_devices; i++)
+						if (input_device[i].desc && !strcmp(buf, input_device[i].desc)) {
+							unlink(name);
+							break;
+						}
+					}
 				}
 			}
-		}
 		closedir(d);
 	}
+}
+
+static pthread_t inmux_task;
+static int inmux_thread_running = 0;
+
+static void count_input_devices(void) {
+	input_device_t *i = input_device;
+	while (i->name)
+		i++, number_of_input_devices++;
+}
+
+static void open_input_devices(void) {
+	time_t now = time(NULL);
+	for (int i = 0; i < number_of_input_devices; i++)
+		if ((input_device[i].fd < 0) && (input_device[i].discovery + 60 < now)) {
+			input_device[i].discovery = now + 60;
+			input_device[i].fd = open(input_device[i].name, O_RDWR | O_NONBLOCK);
+		}
+}
+
+static void close_input_devices(void) {
+	for (int i = 0; i < number_of_input_devices; i++)
+		if (input_device[i].fd > -1) {
+			close(input_device[i].fd);
+			input_device[i].fd = -1;
+		}
+}
+
+static void poll_input_devices(void) {
+	struct pollfd fds[number_of_input_devices];
+	input_device_t inputs[number_of_input_devices];
+	int nfds = 0;
+	for (int i = 1; i < number_of_input_devices; i++)
+		if (input_device[i].fd > -1) {
+			fds[nfds].fd = input_device[i].fd;
+			fds[nfds].events = POLLIN | POLLHUP | POLLERR;
+			fds[nfds].revents = 0;
+			inputs[nfds] = input_device[i];
+			nfds++;
+		}
+
+	if (nfds == 0) {
+		// Only a single input device, which happens to be our master. poll() to avoid looping too fast.
+		fds[0].fd = input_device[0].fd;
+		fds[0].events = POLLIN | POLLHUP | POLLERR;
+		fds[0].revents = 0;
+		poll(fds, 1, 60000 /* ms */);
+		return;
+	}
+
+	int r = poll(fds, nfds, 60000 /* ms */);
+	if (r < 0) {
+		if (errno != EAGAIN)
+			inmux_thread_running = 0;
+		return;
+	}
+	for (int i = 0; i < nfds && r > 0; i++) {
+		if (fds[i].revents & POLLIN) {
+//fprintf(stderr, "### input from fd %d (%s)\n", fds[i].fd, inputs[i].name);
+			struct input_event ev;
+			while (sizeof(ev) == read(fds[i].fd, &ev, sizeof(ev)))
+				write(input_device[0].fd, &ev, sizeof(ev));
+			r--;
+		} else if (fds[i].revents & (POLLHUP | POLLERR | POLLNVAL)) {
+//fprintf(stderr, "### error on %d (%s)\n", fds[i].fd, inputs[i].name);
+			close (fds[i].fd);
+			fds[i].fd = -1;
+			r--;
+		}
+	}
+}
+
+static void *inmux_thread(void *)
+{
+	char threadname[17];
+	strncpy(threadname, __func__, sizeof(threadname));
+	threadname[16] = 0;
+	prctl (PR_SET_NAME, (unsigned long)&threadname);
+
+	inmux_thread_running = 1;
+	while (inmux_thread_running) {
+		open_input_devices();
+		poll_input_devices();
+	}
+
+	return NULL;
+}
+
+void start_inmux_thread(void)
+{
+	input_device[0].fd = open(input_device[0].name, O_RDWR | O_NONBLOCK); // nevis_ir. This is mandatory.
+	if (input_device[0].fd < 0){
+		lt_info("%s: open(%s): %m\n", __func__, input_device[0].name);
+		return;
+	}
+	if (pthread_create(&inmux_task, 0, inmux_thread, NULL) != 0)
+	{
+		lt_info("%s: inmux thread pthread_create: %m\n", __func__);
+		inmux_thread_running = 0;
+		return;
+	}
+	while (!inmux_thread_running)
+		usleep(1000);
+	if (inmux_thread_running == 2)
+		inmux_thread_running = 0;
+}
+
+void stop_inmux_thread(void)
+{
+	if (! inmux_thread_running)
+		return;
+	inmux_thread_running = 0;
+	pthread_kill(inmux_task, SIGKILL);
+	close_input_devices();
+	pthread_join(inmux_task, NULL);
 }
 
 #endif
@@ -127,10 +277,12 @@ void init_td_api()
 			start_input_thread();
 		else
 			lt_info("%s: lircd pidfile present, not starting input thread\n", __func__);
-	}
 #ifdef EVOLUX
-	create_input_devices();
+		count_input_devices();
+		create_input_devices();
+		start_inmux_thread();
 #endif
+	}
 	initialized = true;
 	lt_info("%s end\n", __FUNCTION__);
 }
@@ -138,7 +290,14 @@ void init_td_api()
 void shutdown_td_api()
 {
 	lt_info("%s, initialized = %d\n", __FUNCTION__, (int)initialized);
+#ifdef EVOLUX
+	if (initialized) {
+		stop_input_thread();
+		stop_inmux_thread();
+	}
+#else
 	if (initialized)
 		stop_input_thread();
+#endif
 	initialized = false;
 }
--- tdt/cvs/apps/libstb-hal/libspark/init_lib.h
+++ tdt/cvs/apps/libstb-hal/libspark/init_lib.h
@@ -2,7 +2,4 @@
 #define __INIT_TD_H
 void init_td_api();
 void shutdown_td_api();
-#ifdef EVOLUX
-void create_input_devices();
-#endif
 #endif
