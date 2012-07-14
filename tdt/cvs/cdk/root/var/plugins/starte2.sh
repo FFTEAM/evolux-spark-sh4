74bd78fb7bf74f16a1f868fdf3b353fb695d180c
~pinky1981 <al_borland33@yahoo.de>
2012-07-14 18:47:44 +0200

    rcS update

--- tdt/cvs/cdk/root/var/plugins/starte2.sh
+++ tdt/cvs/cdk/root/var/plugins/starte2.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+if [ -e /etc/enigma2/settings ]; then
 wget -q -O /dev/null "http://127.0.0.1/control/message?popup=E2PLI activated!%20Rebooting now.."
 (sleep 6;rm /etc/.nhd2;touch /etc/.start_enigma2;reboot -f &) &
+fi
 exit
 
