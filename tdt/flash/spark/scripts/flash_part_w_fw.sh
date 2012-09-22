9aef52a49f75bc61c42400ddee27340f18b1b8ee
pinky <al_borland33@yahoo.de>
2012-09-22 19:10:00 +0200

    jffs2 64mb fix

--- tdt/flash/spark/scripts/flash_part_w_fw.sh
+++ tdt/flash/spark/scripts/flash_part_w_fw.sh
@@ -40,34 +40,32 @@ if [ $REPLY == "y" ] || [ $REPLY == "Y" ]; then
 	SIZE=`stat $TUFSBOXDIR/mtd_kernel.pad.bin -t --format %s`
 	SIZE=`printf "0x%x" $SIZE`
 	if [[ $SIZE > "0x799999" ]]; then
-	  echo "KERNEL TO BIG. $SIZE instead of 0x800000" > /dev/stderr
+	  echo "KERNEL TO BIG. $SIZE instead of 0x799999" > /dev/stderr
 	fi
 	mv $TUFSBOXDIR/mtd_kernel.pad.bin $TUFSBOXDIR/uImage
 	rm -f $TUFSBOXDIR/mtd_kernel.pad.bin
 fi
 # --- ROOT ---
 # Size 64MB !
-echo "MKFSJFFS2 --qUfv -p0x4000000 -e0x20000 -r $TMPROOTDIR -o $CURDIR/mtd_root.bin"
-$MKFSJFFS2 -qUfv -p0x4000000 -e0x20000 -r $TMPROOTDIR -o $CURDIR/mtd_root.bin
+echo "MKFSJFFS2 --qUfv -p0x3d09000 -e0x20000 -r $TMPROOTDIR -o $CURDIR/mtd_root.bin"
+$MKFSJFFS2 -qUfv -p0x3d09000 -e0x20000 -r $TMPROOTDIR -o $CURDIR/mtd_root.bin
 echo "SUMTOOL -v -p -e 0x20000 -i $CURDIR/mtd_root.bin -o $CURDIR/mtd_root.sum.bin"
 $SUMTOOL -v -p -e 0x20000 -i $CURDIR/mtd_root.bin -o $CURDIR/mtd_root.sum.bin
-echo "$PAD 0x4000000 $CURDIR/mtd_root.sum.bin $CURDIR/mtd_root.sum.pad.bin"
-$PAD 0x4000000 $CURDIR/mtd_root.sum.bin $CURDIR/mtd_root.sum.pad.bin
+echo "$PAD 0x3d09000 $CURDIR/mtd_root.sum.bin $CURDIR/mtd_root.sum.pad.bin"
+$PAD 0x3d09000 $CURDIR/mtd_root.sum.bin $CURDIR/mtd_root.sum.pad.bin
 
 #echo "MKFSJFFS2 --qUfv -e0x20000 -r $TMPROOTDIR -o $CURDIR/mtd_root.bin"
 #$MKFSJFFS2 -qUfv -e0x20000 -r $TMPROOTDIR -o $CURDIR/mtd_root.bin
 #echo "SUMTOOL -v -p -e 0x20000 -i $CURDIR/mtd_root.bin -o $CURDIR/mtd_root.sum.pad.bin"
 #$SUMTOOL -v -p -e 0x20000 -i $CURDIR/mtd_root.bin -o $CURDIR/mtd_root.sum.pad.bin
 
-rm -f $CURDIR/mtd_root.bin
-rm -f $CURDIR/mtd_root.sum.bin
-
-SIZE=`stat mtd_root.sum.pad.bin -t --format %s`
+SIZE=`stat $CURDIR/mtd_root.sum.pad.bin -t --format %s`
 SIZE=`printf "0x%x" $SIZE`
-if [[ $SIZE > "0x4000000" ]]; then
-  echo "ROOT TO BIG. $SIZE instead of 0x4000000" > /dev/stderr
+if [[ $SIZE > "0x3d09000" ]]; then
+  echo "ROOT TO BIG. $SIZE instead of 0x3d09000" > /dev/stderr
 fi
 
 mv $CURDIR/mtd_root.sum.pad.bin $OUTDIR/e2jffs2.img
-rm -f $CURDIR/mtd_root.sum.pad.bin
+rm -f $CURDIR/mtd_root.bin
+rm -f $CURDIR/mtd_root.sum.bin
 
