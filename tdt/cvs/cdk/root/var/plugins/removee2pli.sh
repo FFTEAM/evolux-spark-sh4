c2446d1da1d74a8e24d0228be84bf174b058d1b1
pinky <pinky@bbook.(none)>
2012-10-19 17:16:37 +0200

    plugin update

--- tdt/cvs/cdk/root/var/plugins/removee2pli.sh
+++ tdt/cvs/cdk/root/var/plugins/removee2pli.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 wget -q -O /dev/null "http://127.0.0.1/control/message?popup=E2PLI deleted!%20Please reboot in 30seconds.."
-(rm -rf /etc/enigma2;rm -rf /usr/lib/enigma2;rm -rf /usr/lib/python2.6;rm -rf /usr/lib/gstreamer-0.10;rm -rf /usr/local/share/enigma2 &) &
+(rm -rf /etc/enigma2;rm -rf /usr/lib/enigma2;rm -rf /usr/lib/python2.6;rm -rf /usr/lib/gstreamer-0.10;rm -rf /usr/local/share/enigma2;rm /etc/.enigma2;if [ -e /etc/.spark1762 ];then touch /etc/.nhd2; else touch /etc/.nhd;fi &) &
 exit
 
