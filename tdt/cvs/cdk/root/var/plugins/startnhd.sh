83cc586482fe8a0d34db5b2213b9d7d00bfe41b1
pinky <pinky@bbook.(none)>
2013-01-24 10:16:44 +0100

    sh update, thx liab

--- tdt/cvs/cdk/root/var/plugins/startnhd.sh
+++ tdt/cvs/cdk/root/var/plugins/startnhd.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 wget -q -O /dev/null "http://127.0.0.1/control/message?popup=NHD activated!%20Rebooting now.."
-(sleep 6;rm /etc/.nhd2;reboot -f &) &
+(sleep 6;rm /etc/.nhd2;touch /etc/.nhd;reboot -f &) &
 exit
 
